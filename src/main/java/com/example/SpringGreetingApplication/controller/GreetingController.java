b61e83a84c2aa7f5c43ae0c7f4a304278758c304
niketajain123 <niketajain236@gmail.com>
2025-02-23 16:17:03 +0530

    [Niketa Jain]Add.Added Greeting Controller.

---
+++ src/main/java/com/example/SpringGreetingApplication/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.SpringGreetingApplication.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String getGreeting() {
+        return "Hello!";
+    }
+}
